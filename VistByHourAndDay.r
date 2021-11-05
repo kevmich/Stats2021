69aa5bf8a85cf53d20a222d1903bd640fb8b92c4
kevmich <kevin.ziebarth@selu.edu>
2021-11-05 16:03:56 -0500

    military time added, still needs order on time

--- VistByHourAndDay.r
+++ VistByHourAndDay.r
@@ -1,15 +1,16 @@
 library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
 data <- read.csv("NewData/Visits-by-hour-and-day-of-the-week-list.csv")
 
 dataframe <- data.frame(avgtime = data[, colnames(data) %in% c('Time')],
                         signInAndOut = data[, colnames(data) %in% c('sign.in.count')])
+
+arrange(dataframe, avgtime)
 print(dataframe)
 
-#print(as.matrix(dataframe[1:6,2]))
-
-#p<-ggplot(dataframe, aes(x=days, y=avgtime)) +
-#  geom_bar(stat="identity", fill="steelblue" )
-#p
+#as.POSIXct(dataframe$avgtime, format="%m/%d/%Y %I:%M:%S%p")
 
 ggplot(dataframe, aes(x = avgtime, y = signInAndOut)) +
   geom_point()
